eef1cc2d107c5c11df362d52fa69129905ff519b
Ahmed Al-Hashimi <Alhashimi346@gmail.com>
2017-04-19 20:59:25 +0000

    Form Submission using JQuery and AJAX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,51 +1,4 @@
 $(document).ready(function () {
-  var data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
-
   function createTweetElement(tweetData) {
     var $tweet = $("<article>").addClass("tweet");
     var $header = $("<header>");
@@ -55,19 +8,45 @@ $(document).ready(function () {
     var $contents = $("<p>").addClass("contents").text(tweetData.content.text);
     var $footer = $("<footer>")
     var $date = $("<p>").addClass("date").text(tweetData.created_at);
-    var $icons = $("<span>").addClass("icons");
+    var $span = $("<span>").addClass("icons");
+    var $icon1 = $("<i>").addClass("fa fa-retweet").attr("aria-hidden","true")
+    var $icon2 = $("<i>").addClass("fa fa-flag").attr("aria-hidden","true")
+    var $icon3 = $("<i>").addClass("fa fa-heart").attr("aria-hidden","true")
     // append elements
     $header.append($avatar, $fullname, $username);
-    $footer.append($date, $icons);
+    $span.append($icon1, $icon2, $icon3);
+    $footer.append($date, $span);
     $tweet.append($header, $contents, $footer);
     return $tweet;
   }
   // function to loop through db and render each element
   function renderTweets(tweets) {
+    // in order not to get tweets dublicated
+    $('section#tweets-container').empty();
+    // loop through db and render each element
     tweets.forEach(function(tweet){
-      $('section#tweets-container').append(createTweetElement(tweet));
+      $('section#tweets-container').prepend(createTweetElement(tweet));
     });
   }
-  renderTweets(data)
-
+  //loading tweets from db
+  function loadTweets() {
+    $.ajax({
+      method: "GET",
+      url: "/tweets",
+    }).then(function(tweets){
+      renderTweets(tweets);
+    })
+  }
+  // handle submition form
+  $('.new-tweet form').on("submit", function(event){
+    event.preventDefault();
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      data: $(this).serialize()
+    }).then(function () {
+      loadTweets();
+    });
+  });
+loadTweets();
 });
