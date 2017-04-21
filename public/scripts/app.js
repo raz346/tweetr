fb3c04ceaf76071b3a6d1373b796ea153e10dd9a
Ahmed Al-Hashimi <Alhashimi346@gmail.com>
2017-04-21 20:33:08 +0000

    little polish

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,16 +2,17 @@ $(document).ready(function () {
   function createTweetElement(tweetData) {
     var $tweet = $("<article>").addClass("tweet");
     var $header = $("<header>");
-    var $avatar = $("<img>").addClass("avatar").attr("src",tweetData.user.avatars.small);
+    var $avatar = $("<img>").addClass("avatar").attr("src", tweetData.user.avatars.small);
     var $fullname = $("<h3>").addClass("fullname").text(tweetData.user.name);
     var $username = $("<p>").addClass("username").text(tweetData.user.handle);
     var $contents = $("<p>").addClass("contents").text(tweetData.content.text);
-    var $footer = $("<footer>")
-    var $date = $("<p>").addClass("date").text(tweetData.created_at);
+    var $footer = $("<footer>");
+    var date = new Date(tweetData.created_at);
+    var $date = $("<p>").addClass("date").text(date);
     var $span = $("<span>").addClass("icons");
-    var $icon1 = $("<i>").addClass("fa fa-retweet").attr("aria-hidden","true")
-    var $icon2 = $("<i>").addClass("fa fa-flag").attr("aria-hidden","true")
-    var $icon3 = $("<i>").addClass("fa fa-heart").attr("aria-hidden","true")
+    var $icon1 = $("<i>").addClass("fa fa-retweet").attr("aria-hidden", "true");
+    var $icon2 = $("<i>").addClass("fa fa-flag").attr("aria-hidden", "true");
+    var $icon3 = $("<i>").addClass("fa fa-heart").attr("aria-hidden", "true");
     // append elements
     $header.append($avatar, $fullname, $username);
     $span.append($icon1, $icon2, $icon3);
@@ -32,36 +33,36 @@ $(document).ready(function () {
   function loadTweets() {
     $.ajax({
       method: "GET",
-      url: "/tweets",
+      url: "/tweets"
     }).then(function(tweets){
       renderTweets(tweets);
-    })
+    });
   }
   // handle submition
-    $(".new-tweet form").on("submit", function(event){
-      event.preventDefault();
-      var charCount = $(".new-tweet textarea").val().length;
-      if (charCount === 0 ) {
-        alert("please enter a tweet");
-        return;
-      } else if (charCount > 140) {
-        alert("your tweet is too long")
-        return;
-      } else {
-        $.ajax({
-          method: "POST",
-          url: "/tweets",
-          data: $(this).serialize()
-        }).then(function () {
-          loadTweets();
-        });
-      }
-    });
+  $(".new-tweet form").on("submit", function(event){
+    event.preventDefault();
+    var charCount = $(".new-tweet textarea").val().length;
+    if (charCount === 0 ) {
+      alert("please enter a tweet");
+      return;
+    } else if (charCount > 140) {
+      alert("your tweet is too long");
+      return;
+    } else {
+      $.ajax({
+        method: "POST",
+        url: "/tweets",
+        data: $(this).serialize()
+      }).then(function () {
+        loadTweets();
+      });
+    }
+    $(".new-tweet textarea").val("");
+  });
   loadTweets();
 
   $("#nav-bar .compose").on("click", function(event){
     $(".new-tweet").slideToggle();
     $(".new-tweet textarea").focus();
-
   });
 });
